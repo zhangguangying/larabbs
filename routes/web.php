1a1fb16fcfbd6620a1ae1cb4cfa4ff8c8571fa07
zhangguangying <hyuiing@163.com>
2018-08-26 04:11:55 +0000

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,6 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 // 用户
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 分类
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
