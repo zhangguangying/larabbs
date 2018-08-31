94a6e267c2d94c4c81b671d92f0a7e7eb140924f
zhangguangying <786781353@qq.com>
2018-08-31 06:49:39 +0000

    邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 
 use App\Models\Reply;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification 
 {
     use Queueable;
 
@@ -34,7 +34,7 @@ class TopicReplied extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
@@ -58,6 +58,15 @@ class TopicReplied extends Notification
             'topic_id' => $topic->id,
             'topic_title' => $topic->title,
         ];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+                    ->line('你的话题有新回复！')
+                    ->action('查看回复', $url);
     }
 
     /**
--- resources/views/replies/_reply_list.blade.php
+++ resources/views/replies/_reply_list.blade.php
@@ -15,9 +15,14 @@
                 <span> · </span>
                 <span class="meta" title="{{ $reply->created_at }}">{{ $reply->created_at->diffForHumans() }}</span>
                 <span class="meta pull-right">
-                    <a href="" title="删除回复">
-                        <span class="glyphicon glyphicon-trash"></span>
-                    </a>
+                    <form action="{{ route('replies.destroy', $reply->id) }}" method="post">
+                        {{ csrf_field() }}
+                        {{ method('delete') }}
+
+                        <button class="btn btn-default btn-xs pull-left">
+                            <i class="glyphicon glyphicon-trash"></i>
+                        </button>
+                    </form>
                 </span>
             </div>
 
