4dec7902ad59b56ed8c1c12bbcba5f5f21ca994b
zhangguangying <786781353@qq.com>
2018-09-02 05:19:15 +0000

    集成laravel-permission扩展包

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -39,5 +39,11 @@ class UsersTableSeeder extends Seeder
         $user->password = bcrypt('123456');
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
+        // 为1号用户分配角色
+        $user->assignRole('Founder');
+
+        // 为2号用户分配角色
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
