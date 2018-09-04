2106faae3a997f11705c6f87e205e431ae211860
zhangguangying <786781353@qq.com>
2018-09-04 11:04:06 +0000

    后台站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="">张光应</a> 著 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="">联系我们</a></p>
+        <p class="pull-right"><a href="mailto: {{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'LaraBBS') - Laravel进阶教程</title>
-    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')">
+    <meta name="description" content="@yield('description', setting('seo_keyword', 'LaraBBS 爱好者社区。'))">
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛'))">
 
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     @yield('styles')
