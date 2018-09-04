abed741d6e93282f32287424e24b772fa0e9e4e2
zhangguangying <786781353@qq.com>
2018-09-04 11:42:54 +0000

    后台权限控制

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,6 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 // 通知
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
+// 无权限登录
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
